89367b3c0d40c742a832b6e0983c61dffb660cc1
ulicskagergo <ulicskagergo@gmail.com>
2021-02-25 21:12:49 +0100

    Add Tachograph test

---
+++ train-tachograph/src/test/java/hu/bme/mit/train/tachograph/TrainTachographTest.java
@@ -0,0 +1,16 @@
+package hu.bme.mit.train.tachograph;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+public class TrainTachographTest {
+    @Test
+    public void TestIfCollectionHasSomeElements(){
+        TrainTachograph tachograph = new TrainTachograph();
+        tachograph.AddToTachograph(3, 5);
+        tachograph.AddToTachograph(4, 50000);
+        tachograph.AddToTachograph(50000, 5);
+
+        Assert.assertNotNull(tachograph.GetFromTachograph());
+    }
+}
