25e6bf35a89d904cabc0e35fe3cd6986fc504713
kinga <szabo.kinga1999@edu.bme.hu>
2021-02-26 20:42:51 +0100

    fix build error

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -49,11 +49,5 @@ public class TrainSystemTest {
 		controller.followSpeed();
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
-
-	@Test
-	public void NewJUnitTest() {
-		Assert.assertEquals(5,sensor.getSpeedLimit());
-	}
-
 	
 }
