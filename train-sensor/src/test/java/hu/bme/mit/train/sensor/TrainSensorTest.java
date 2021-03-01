60712e8f5ee21bde0d4174e4c1ed5532b876bc3d
patrick-nanys <patrick.nanys2000@gmail.com>
2021-03-01 15:42:04 +0100

    Fix broken test and add another one

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -11,14 +11,35 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensorImpl sensor;
+    TrainController controller;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        controller = new TrainController() {
+            @Override
+            public void followSpeed() {}
+
+            @Override
+            public int getReferenceSpeed() { return 0; }
+
+            @Override
+            public void setSpeedLimit(int speedLimit) {}
+
+            @Override
+            public void setJoystickPosition(int joystickPosition) {}
+        };
+        sensor = new TrainSensorImpl(controller, null);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        TrainSensorImpl sensor = new TrainSensorImpl(null, null);
-        assert sensor.getSpeedLimit() == 5;
+    public void testDefaultSensorValue() {
+        assert sensor.getSpeedLimit() == 10;
+    }
+
+    @Test
+    public void testSettingSensorValue() {
+        sensor.overrideSpeedLimit(12);
+        assert sensor.getSpeedLimit() == 12;
     }
 }
