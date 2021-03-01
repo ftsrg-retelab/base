ab33c9df5b8821cbea6338dc5bcbb05aba3ea40b
Flóra <Flra@DESKTOP-QPMNBGS>
2021-03-01 22:03:27 +0100

    tet

--- train-controller/src/main/java/hu/bme/mit/train/controller/TachographTest.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TachographTest.java
@@ -7,8 +7,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.*;
+
+import java.sql.Time;
 
 
 
