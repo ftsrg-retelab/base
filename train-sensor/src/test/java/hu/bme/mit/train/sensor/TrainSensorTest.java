1bb988bcb9668779d9339be8e94ba14627d9ce81
ulicskagergo <ulicskagergo@gmail.com>
2021-03-03 20:40:23 +0100

    Finish train sensor test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,32 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.user.TrainUserImpl;
+import hu.bme.mit.train.controller.TrainControllerImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainController controller;
+    TrainUser user;
+    TrainSensor sensor;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        controller = new TrainControllerImpl();
+        user = new TrainUserImpl(controller);
+        sensor = new TrainSensorImpl(controller, user);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void TestSpeedOverride() {
+        sensor.overrideSpeedLimit(33);
+        Assert.assertEquals(33, sensor.getSpeedLimit());
     }
 }
