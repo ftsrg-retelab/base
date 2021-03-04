96dd3095c6602f5918577acd1448b1303043953f
HP_OMEN <HP_OMEN@192.168.0.101>
2021-03-04 12:36:33 +0100

    Build error commit

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -1,13 +1,6 @@
 package hu.bme.mit.train.system;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
-import hu.bme.mit.train.interfaces.TrainController;
-import hu.bme.mit.train.interfaces.TrainSensor;
-import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.system.TrainSystem;
 
 public class TrainSystemTest {
 
