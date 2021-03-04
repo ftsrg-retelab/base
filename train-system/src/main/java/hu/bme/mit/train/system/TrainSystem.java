cee3aa6301f40fa3bf838900edef7bb718759784
fkonrad97 <fkonrad97@gmail.com>
2021-03-04 14:04:50 +0100

    import

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -6,12 +6,14 @@ import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
+import java.time.LocalDateTime;
 
 public class TrainSystem {
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+	private LocalDateTime now = LocalDateTime.now();
 
 	public TrainController getController() {
 		return controller;
