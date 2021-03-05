127ee71867e64ae8aa8dcba475d499782c07d1dc
Hain Balázs <hainb@sch.bme.hu>
2021-03-05 19:14:04 +0100

    added a unit test for train-sensor

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,29 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.controller.TrainControllerImpl;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.user.TrainUserImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    private TrainController controller;
+    private TrainSensorImpl ts;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        TrainController controller = new TrainControllerImpl();
+        ts =  new TrainSensorImpl(controller, new TrainUserImpl(controller));
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void SpeedLimitTest() {
+        assertEquals(5, ts.getSpeedLimit());
     }
 }
