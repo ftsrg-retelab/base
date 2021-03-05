d6567a16270ae28b2ac462cf52b08b2f06bd1556
kapcsi <50232060+kapcsi@users.noreply.github.com>
2021-03-05 16:12:16 +0100

    reveret

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -33,6 +33,7 @@ public class TrainControllerImpl implements TrainController {
 		this.speedLimit = speedLimit;
 		enforceSpeedLimit();
 
+	}
 
 	private void enforceSpeedLimit() {
 		if (referenceSpeed > speedLimit) {
