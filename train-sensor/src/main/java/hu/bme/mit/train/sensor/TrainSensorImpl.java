671cacc2b1ce04b318b76f4875e9988a4305198f
SteveP1999 <halabe@freemail.hu>
2021-03-06 20:19:27 +0100

    i added a mistake in TrainSensorImpl

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -10,6 +10,7 @@ public class TrainSensorImpl implements TrainSensor {
 	private TrainUser user;
 	private int speedLimit = 5;
 	private int constant = 2;
+	priv int;
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
