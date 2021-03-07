8c0dad407c13752490cfd49aeebacc076e491366
Puskás Levente <puskas.levente99@gmail.com>
2021-03-07 16:32:17 +0100

    Changed a line in TrainController class

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,7 +7,7 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-
+	private int counter = 0;
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
