d4fd41db5fffe7aadc6dbc972b289c2a20fd37b9
SteveP1999 <halabe@freemail.hu>
2021-03-07 20:18:23 +0100

    Kijavítottam a hibát és addoltam egy testet

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,7 +7,6 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-	priv asd;
 
 	@Override
 	public void followSpeed() {
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,24 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainSensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensorImpl tsi;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        tsi = new TrainSensorImpl(null, null);
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+        assertEquals(tsi.getSpeedLimit(), 5);
     }
 }
