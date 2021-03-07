b22e2f1d2d1d2da1c38c6c63fa1afa9c1b99acdd
Balázs Pintér <pntrblzs21@gmail.com>
2021-03-07 11:37:07 +0100

    Sensor Implementation fixed, extra unit test added

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -22,7 +22,7 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
-		this.speedLimit = speed Limit;
+		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
 		System.out.println("speedlimit reached!");
 	}
--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,18 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+	public void OverridingSpeedLimit_DecreasesReferenceSpeed() {
+		sensor.overrideSpeedLimit(10);
+		Assert.assertEquals(0, controller.getReferenceSpeed());
+
+		user.overrideJoystickPosition(10);
+
+		controller.followSpeed();
+		Assert.assertEquals(10, controller.getReferenceSpeed());
+		sensor.overrideSpeedLimit(5);
+		controller.followSpeed();
+		Assert.assertEquals(5, controller.getReferenceSpeed());
+	}
 	
 }
