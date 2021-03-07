443ddea559806703a98bf6aec551a1dd5fa2e6f2
BLilla <ballalilla0409@gmail.com>
2021-03-07 03:27:48 +0100

    TrainSensorTest repaired

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,6 +3,9 @@ package hu.bme.mit.train.sensor;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.user.TrainUserImpl;
+import hu.bme.mit.train.controller.TrainControllerImpl;
+import hu.bme.mit.train.sensor.TrainSensorImpl;
 
 import org.junit.Assert;
 import org.junit.Before;
