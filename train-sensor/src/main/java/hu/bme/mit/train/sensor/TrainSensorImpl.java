adeb7d87cf041c20b5b4827c7fef7eceab177c2d
harasztikamea <harasztikamea@edu.bme.hu>
2021-03-08 20:49:00 +0100

    Added a plus line in constructor

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -12,6 +12,7 @@ public class TrainSensorImpl implements TrainSensor {
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
+		
 		this.user = user;
 	}
 
