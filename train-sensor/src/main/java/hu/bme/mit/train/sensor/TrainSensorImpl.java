0d659d81596e81dc0ceb14e6cb8d8e63e8ae312f
harasztikamea <harasztikamea@edu.bme.hu>
2021-03-08 20:50:03 +0100

    Added println the same line

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -12,7 +12,7 @@ public class TrainSensorImpl implements TrainSensor {
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
-		
+		System.out.println("error");
 		this.user = user;
 	}
 
