7fec4c6a2b9cec68dbf64f2305d8aaba0d340716
Roland Toth <troland703@gmail.com>
2021-03-08 22:22:00 +0100

    Created build error

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,10 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+	public void generateBuildError() {
+		Assert.asserTrue(false);
+	}
+
 	
 }
