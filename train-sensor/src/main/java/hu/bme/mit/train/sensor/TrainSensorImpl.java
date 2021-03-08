650d39851b08a581ec19b9f85b78a49e3daa3336
Roland Toth <troland703@gmail.com>
2021-03-08 21:19:36 +0100

    Edited speed limit

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -8,7 +8,7 @@ public class TrainSensorImpl implements TrainSensor {
 
 	private TrainController controller;
 	private TrainUser user;
-	private int speedLimit = 10;
+	private int speedLimit = 8;
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
