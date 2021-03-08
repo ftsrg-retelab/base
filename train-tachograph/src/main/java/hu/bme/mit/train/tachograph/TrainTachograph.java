383c2e3c836e0cba4f0b4c824ad1ad496169f28a
BLilla <ballalilla0409@gmail.com>
2021-03-08 11:58:17 +0100

    Fix tachograph module implementation

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -1,11 +1,5 @@
 package hu.bme.mit.train.tachograph;
 
-import hu.bme.mit.train.controller.TrainControllerImpl;
-import hu.bme.mit.train.interfaces.TrainController;
-import hu.bme.mit.train.interfaces.TrainSensor;
-import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.sensor.TrainSensorImpl;
-import hu.bme.mit.train.user.TrainUserImpl;
 
 import java.time.LocalTime;
 import com.google.common.collect.HashBasedTable;
@@ -14,14 +8,10 @@ import com.google.common.collect.Table;
 
 public class TrainTachograph {
 	
-	private TrainController controller;
-	private TrainUser user;
 	private Table<LocalTime, Integer, Integer> logTable;
 	
 	
-	public Tachograph() {
-		controller = new TrainControllerImpl();
-		user = new TrainUserImpl(controller);
+	public TrainTachograph() {
 		logTable = HashedBaseTable.create();
 	}
 	
