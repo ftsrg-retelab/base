86e384624a9821ecd190135f29b66e2ab6fe88fd
fdominik98 <frey.dominik@hotmail.hu>
2021-03-17 17:10:27 +0100

    Implementing reference speed change in Main

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 import hu.bme.mit.train.system.TrainSystem;
 
 public class Main{
+
+  Timer timer = new Timer("Timer");
+
+
   public static void main(String[] arg){
     TrainSystem system = new TrainSystem();
-    system.getController().setJoystickPosition(0);    
+    system.getController().setJoystickPosition(0);
+
+
+    TimerTask task = new TimerTask() {
+      public void run() {
+        system.getController().followSpeed();
+      }
+    };
+    timer.schedule(task, 0, 0.5);
+
   }  
 
 }
