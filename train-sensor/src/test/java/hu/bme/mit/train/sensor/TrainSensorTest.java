3f850bd4843be5d3556d2e7485b03e603492cb8a
ulicskagergo <ulicskagergo@gmail.com>
2021-03-22 16:56:10 +0100

    Convert tests into Mockito tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,8 +3,6 @@ package hu.bme.mit.train.sensor;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.user.TrainUserImpl;
-import hu.bme.mit.train.controller.TrainControllerImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,46 +17,35 @@ public class TrainSensorTest {
 
     @Before
     public void before() {
-        controller = new TrainControllerImpl();
-        user = new TrainUserImpl(controller);
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
         sensor = new TrainSensorImpl(controller, user);
-    }
-
-    @Test
-    public void TestSpeedOverride() {
-        sensor.overrideSpeedLimit(33);
-        Assert.assertEquals(33, sensor.getSpeedLimit());
     }
 
     @Test
     public void TestOverrideSpeedLimitAbsoluteMarginForNull() {
         sensor.overrideSpeedLimit(0);
-        Assert.assertFalse(user.getAlarmState());
+        verify(user, times(0)).setAlarmState(true);
     }
 
     @Test
     public void TestOverrideSpeedLimitAbsoluteMarginForTrue() {
         sensor.overrideSpeedLimit(501);
-        Assert.assertTrue(user.getAlarmState());
+        verify(user, times(1)).setAlarmState(true);
     }
 
     @Test
     public void TestOverrideSpeedLimitRelativeMarginForTrue() {
-        controller.setSpeedLimit(150);
-        user.overrideJoystickPosition(150);
-        controller.followSpeed();
-
+        when(controller.getReferenceSpeed()).thenReturn(150);
         sensor.overrideSpeedLimit(50);
-        Assert.assertTrue(user.getAlarmState());
+        verify(user, times(1)).setAlarmState(true);
     }
 
     @Test
     public void TestOverrideSpeedLimitRelativeMarginForFalse() {
-        controller.setSpeedLimit(150);
-        user.overrideJoystickPosition(150);
-        controller.followSpeed();
-
+        when(controller.getReferenceSpeed()).thenReturn(150);
         sensor.overrideSpeedLimit(100);
-        Assert.assertFalse(user.getAlarmState());
+        verify(user, times(0)).setAlarmState(true);
     }
+
 }
