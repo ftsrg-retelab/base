3c30b727a4ed32c912a651d352ab75e5f3cace31
Bence56 <bsfcb002@gmail.com>
2021-03-24 21:18:02 +0100

    Implemented through a test

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -9,46 +9,86 @@ import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.system.TrainSystem;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainSystemTest {
 
-	TrainController controller;
-	TrainSensor sensor;
-	TrainUser user;
-	
-	@Before
-	public void before() {
-		TrainSystem system = new TrainSystem();
-		controller = system.getController();
-		sensor = system.getSensor();
-		user = system.getUser();
+    TrainController controller;
+    TrainSensor sensor;
+    TrainUser user;
 
-		sensor.overrideSpeedLimit(50);
-	}
-	
-	@Test
-	public void OverridingJoystickPosition_IncreasesReferenceSpeed() {
-		sensor.overrideSpeedLimit(10);
+    @Before
+    public void before() {
+        TrainSystem system = new TrainSystem();
+        controller = system.getController();
+        sensor = system.getSensor();
+        user = system.getUser();
 
-		Assert.assertEquals(0, controller.getReferenceSpeed());
-		
-		user.overrideJoystickPosition(5);
+        sensor.overrideSpeedLimit(50);
+    }
 
-		controller.followSpeed();
-		Assert.assertEquals(5, controller.getReferenceSpeed());
-		controller.followSpeed();
-		Assert.assertEquals(10, controller.getReferenceSpeed());
-		controller.followSpeed();
-		Assert.assertEquals(10, controller.getReferenceSpeed());
-	}
+    @Test
+    public void OverridingJoystickPosition_IncreasesReferenceSpeed() {
+        sensor.overrideSpeedLimit(10);
 
-	@Test
-	public void OverridingJoystickPositionToNegative_SetsReferenceSpeedToZero() {
-		user.overrideJoystickPosition(4);
-		controller.followSpeed();
-		user.overrideJoystickPosition(-5);
-		controller.followSpeed();
-		Assert.assertEquals(0, controller.getReferenceSpeed());
-	}
+        Assert.assertEquals(0, controller.getReferenceSpeed());
 
-	
+        user.overrideJoystickPosition(5);
+
+        controller.followSpeed();
+        Assert.assertEquals(5, controller.getReferenceSpeed());
+        controller.followSpeed();
+        Assert.assertEquals(10, controller.getReferenceSpeed());
+        controller.followSpeed();
+        Assert.assertEquals(10, controller.getReferenceSpeed());
+    }
+
+    @Test
+    public void OverridingJoystickPositionToNegative_SetsReferenceSpeedToZero() {
+        user.overrideJoystickPosition(4);
+        controller.followSpeed();
+        user.overrideJoystickPosition(-5);
+        controller.followSpeed();
+        Assert.assertEquals(0, controller.getReferenceSpeed());
+    }
+
+    @Test
+    public void UserChangesJoystickPositionTest() {
+        TimerTask timerTaskAccelerate = new TimerTask() {
+            @Override
+            public void run() {
+                user.overrideJoystickPosition(3);
+                controller.followSpeed();
+                Assert.assertEquals(3, controller.getReferenceSpeed());
+            }
+        };
+
+        TimerTask timerTaskDecelerate = new TimerTask() {
+            @Override
+            public void run() {
+                user.overrideJoystickPosition(5);
+                controller.followSpeed();
+                Assert.assertEquals(5, controller.getReferenceSpeed());
+                user.overrideJoystickPosition(-1);
+                Assert.assertEquals(4, controller.getReferenceSpeed());
+            }
+        };
+        TimerTask timerTaskHold = new TimerTask() {
+            @Override
+            public void run() {
+                user.overrideJoystickPosition(4);
+                controller.followSpeed();
+                Assert.assertEquals(4, controller.getReferenceSpeed());
+                user.overrideJoystickPosition(0);
+                Assert.assertEquals(4, controller.getReferenceSpeed());
+            }
+        };
+        long delay = 100L;
+        Timer timer = new Timer("Timer");
+        timer.schedule(timerTaskAccelerate, delay);
+        timer.schedule(timerTaskDecelerate,delay);
+        timer.schedule(timerTaskHold,delay);
+    }
+
 }
