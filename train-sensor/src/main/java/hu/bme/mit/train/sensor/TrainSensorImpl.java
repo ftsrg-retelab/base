f4d4a06e42d05613c55a95d19733d3c707c038b9
Erik <csat.erk@gmail.com>
2021-03-24 22:32:50 +0100

    corrected code smells

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -2,20 +2,20 @@ package hu.bme.mit.train.sensor;
 
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
-import hu.bme.mit.train.interfaces.TrainUser;
 
 public class TrainSensorImpl implements TrainSensor {
 
 	private TrainController controller;
-	private TrainUser user;
+
 	private int speedLimit = 5;
 
-	public TrainSensorImpl(TrainController controller, TrainUser user) {
+	public TrainSensorImpl(TrainController controller) {
 		this.controller = controller;
-		this.user = user;
+
 	}
 
-	@Override
+
+    @Override
 	public int getSpeedLimit() {
 		return speedLimit;
 	}
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,47 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+
 
 public class TrainSensorTest {
 
+    TrainController controller;
+    TrainSensor sensor;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        controller=new TrainController() {
+            @Override
+            public void followSpeed() {
+
+            }
+
+            @Override
+            public int getReferenceSpeed() {
+                return 0;
+            }
+
+            @Override
+            public void setSpeedLimit(int speedLimit) {
+            }
+
+            @Override
+            public void setJoystickPosition(int joystickPosition) {
+            }
+        };
+
+        sensor=new TrainSensorImpl(controller);
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+        assertEquals(5, sensor.getSpeedLimit());
     }
 }
