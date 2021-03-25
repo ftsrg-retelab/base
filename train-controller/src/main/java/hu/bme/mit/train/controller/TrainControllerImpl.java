49bfc872c05466b5ecc65dc5d5682fe9989c731c
Mapika <marosimark251@gmail.com>
2021-03-25 17:22:17 +0100

    new timer based UpdateSpeed

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,21 +2,39 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+class UpdateSpeed extends TimerTask {
+	private TrainController trainController;
+
+	public UpdateSpeed(TrainController trainController) {
+		this.trainController = trainController;
+	}
+
+	@Override
+	public void run() {
+		this.trainController.followSpeed();
+	}
+}
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Timer timer;
 
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
 			referenceSpeed = 0;
 		} else {
-		    if(referenceSpeed+step > 0) {
-                referenceSpeed += step;
-            } else {
-		        referenceSpeed = 0;
+			if (referenceSpeed + step > 0) {
+				referenceSpeed += step;
+			} else {
+				referenceSpeed = 0;
             }
 		}
 
@@ -43,7 +61,11 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
+	}
+
+	public void seedUpdateTimerStart(int l) {
+		timer.schedule(new UpdateSpeed(this), l);
 	}
 
 }
