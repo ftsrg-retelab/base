bc487da8554365f73b6ccb0213ff26fab548b60b
kissbeni <kissbenedek99@gmail.com>
2021-03-29 18:17:29 +0200

    add timer feature

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,25 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
     private int step = 0;
     private int referenceSpeed = 0;
     private int speedLimit = 0;
+
+    private final Timer theTimer = new Timer();
+
+    public TrainControllerImpl() {
+        theTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                followSpeed();
+            }
+        }, 0, 100);
+    }
 
     @Override
     public void followSpeed() {
