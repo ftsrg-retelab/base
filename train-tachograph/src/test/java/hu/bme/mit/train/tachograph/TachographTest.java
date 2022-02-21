0731c441829b7c801b59a5e5f32fcd9df64258f7
Zalán Demeter <zalandemeter12@gmail.com>
2022-02-21 16:17:17 +0100

    unit test values changed

--- train-tachograph/src/test/java/hu/bme/mit/train/tachograph/TachographTest.java
+++ train-tachograph/src/test/java/hu/bme/mit/train/tachograph/TachographTest.java
@@ -7,10 +7,7 @@ public class TachographTest {
     @Test
     public void TestNotEmpty(){
         TrainTachograph tachograph = new TrainTachograph();
-        tachograph.addToTachograph(3, 5);
-        tachograph.addToTachograph(4, 50000);
-        tachograph.addToTachograph(50000, 5);
-
+        tachograph.addToTachograph(15, 6);
         Assert.assertFalse(tachograph.isEmpty());
     }
 }
