d2cbf51c5e8b88463749cbeebf17937e7aeef1f4
damenija7 <damenija7@gmail.com>
2022-02-22 22:40:08 +0100

    Added TrainSystemAlternativeTest

---
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemAlternativeTest.java
@@ -0,0 +1,56 @@
+package hu.bme.mit.train.system;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrainSystemAlternativeTest {
+
+	TrainController controller;
+	TrainSensor sensor;
+	TrainUser user;
+	
+	@Before
+	public void before() {
+		TrainSystem system = new TrainSystem();
+		controller = system.getController();
+		sensor = system.getSensor();
+		user = system.getUser();
+
+		sensor.overrideSpeedLimit(100);
+	}
+	
+	@Test
+	public void OverridingJoystickPosition_IncreasesReferenceSpeed() {
+		sensor.overrideSpeedLimit(20);
+
+		Assert.assertEquals(0, controller.getReferenceSpeed());
+		
+		user.overrideJoystickPosition(5);
+
+		controller.followSpeed();
+		Assert.assertEquals(5, controller.getReferenceSpeed());
+		controller.followSpeed();
+		Assert.assertEquals(10, controller.getReferenceSpeed());
+		controller.followSpeed();
+		Assert.assertEquals(15, controller.getReferenceSpeed());
+		controller.followSpeed();
+		Assert.assertEquals(20, controller.getReferenceSpeed());
+		controller.followSpeed();
+		Assert.assertEquals(20, controller.getReferenceSpeed());
+	}
+
+	@Test
+	public void OverridingJoystickPositionToNegative_SetsReferenceSpeedToZero() {
+		user.overrideJoystickPosition(4);
+		controller.followSpeed();
+		user.overrideJoystickPosition(-5);
+		controller.followSpeed();
+		Assert.assertEquals(0, controller.getReferenceSpeed());
+	}
+
+	
+}
