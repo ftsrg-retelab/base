97c24403bbd0a24466039b87ade07f5d7e6be3b4
Virág <baski.virag@edu.bme.hu>
2022-02-26 12:42:15 +0100

    tachograf module

---
+++ tachograf/src/main/java/hu/bme/mit/train/system/Tachograf_Sys.java
@@ -0,0 +1,27 @@
+package main.java.hu.bme.mit.train.system;
+
+import hu.bme.mit.train.controller.TrainControllerImpl;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.sensor.TrainSensorImpl;
+import hu.bme.mit.train.user.TrainUserImpl;
+
+public class Tachograf_Sys {
+
+	private TrainController controller = new TrainControllerImpl();
+	private TrainUser user = new TrainUserImpl(controller);
+	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+
+	public TrainController getController() {
+		return controller;
+	}
+
+	public TrainSensor getSensor() {
+		return sensor;
+	}
+
+	public TrainUser getUser() {
+		return user;
+	}
+}
