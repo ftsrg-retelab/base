d1c8461128063db6c6457ba4d9dfb51437ca99fd
Virág <baski.virag@edu.bme.hu>
2022-02-26 12:11:21 +0100

    decreaserefspeed junit test

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -54,14 +54,14 @@ public class TrainSystemTest {
 	public void OverridingJoystickPosition_DecreasesReferenceSpeed() {
 		sensor.overrideSpeedLimit(10);
 
-		Assert.assertEquals(0, controller.getReferenceSpeed());
+		Assert.assertEquals(10, controller.getReferenceSpeed());
 
 		user.overrideJoystickPosition(-5);
 
 		controller.followSpeed();
-		Assert.assertEquals(10, controller.getReferenceSpeed());
-		controller.followSpeed();
 		Assert.assertEquals(5, controller.getReferenceSpeed());
+		controller.followSpeed();
+		Assert.assertEquals(0, controller.getReferenceSpeed());
 		controller.followSpeed();
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
