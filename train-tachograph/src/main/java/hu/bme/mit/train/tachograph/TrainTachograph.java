0b6f89a8f32dded4433c0ce476d96e74e353508d
ancsibende <ancsi.bende@edu.bme.hu>
2022-02-26 21:12:10 +0100

    tachograph update & test

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -1,50 +1,30 @@
 package hu.bme.mit.train.tachograph;
 
+import java.util.Date;
+
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
+import com.google.common.collect.Tables;
 import hu.bme.mit.train.controller.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainController;
-import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
-import java.time.LocalTime;
-
 public class TrainTachograph {
-
-    private TrainController controller = new TrainControllerImpl();
-    private TrainUser user = new TrainUserImpl(controller);
-    private TrainSensor sensor = new TrainSensorImpl(controller, user);
-    private Table<LocalTime, Integer, Integer> infoTable;
+    private TrainController trainController;
+    private TrainUser trainUser;
+    private Table<Date, Integer, Integer> recordTable = HashBasedTable.create();
 
     public TrainTachograph() {
-        infoTable
-                = HashBasedTable.create();
-
-        infoTable.put(java.time.LocalTime.now(), user.getJoystickPosition(), controller.getReferenceSpeed());
+        trainController = new TrainControllerImpl();
+        trainUser = new TrainUserImpl(trainController);
     }
 
-    public void AddValuesToTachograph(LocalTime time, Integer joystickPosition, Integer referenceSpeed) {
-        infoTable.put(
-                time,
-                joystickPosition,
-                referenceSpeed
-        );
+    public Table<Date, Integer, Integer> getRecordTable() {
+        return Tables.unmodifiableTable(recordTable);
     }
 
-    public boolean TableIsEmpty() { return infoTable.isEmpty(); }
-
-    public TrainController getController() {
-        return controller;
+    public void record() {
+        recordTable.put(new Date(), trainUser.getJoystickPosition(), trainController.getReferenceSpeed());
     }
-
-    public TrainSensor getSensor() {
-        return sensor;
-    }
-
-    public TrainUser getUser() {
-        return user;
-    }
-
 }
