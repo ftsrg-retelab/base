8a2cafc37cd09cf5f1af9cb6be8822691d0d482f
birollaci <birollaci@gmail.com>
2022-02-27 15:27:04 +0100

    6. feladatsor - tachograf

---
+++ train-tachograf/src/main/java/hu/bme/mit/train/tachograf/TrainTachograf.java
@@ -0,0 +1,21 @@
+package hu.bme.mit.train.tachograf;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+public class TrainTachograf {
+
+	private TrainController c = new TrainController();
+	private TainUser u = new TainUser();
+	private Table<Date, Integer, Integer> tachografTable = HashBasedTable.create();
+	
+	tachografTable.put(new Date(), u.getJoystickPosition(), c.getReferenceSpeed());
+	
+	public Table<Date, Integer, Integer> getTachografTable() {
+		return tachografTable;
+	}
+
+}
