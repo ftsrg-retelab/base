eec71986183d081a9f9e78ed0326bca8ca8ba078
birollaci <birollaci@gmail.com>
2022-02-27 12:16:09 +0100

    add controller test.

---
+++ train-controller/src/test/java/hu/bme/mit/train/controller/TrainControllerTest.java
@@ -0,0 +1,14 @@
+package hu.bme.mit.train.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class TrainControllerTest {
+
+    @Test
+    public void Test() {
+        TrainControllerImpl = new TrainControllerImpl();
+    }
+}
