35e3f5642911fc5c3948f7b02a0338f643d82428
birollaci <birollaci@gmail.com>
2022-02-27 16:23:09 +0100

    fixing

--- train-tachograf/src/main/java/hu/bme/mit/train/tachograf/TrainTachograf.java
+++ train-tachograf/src/main/java/hu/bme/mit/train/tachograf/TrainTachograf.java
@@ -9,7 +9,7 @@ import com.google.common.collect.Table;
 public class TrainTachograf {
 
 	private TrainControllerImpl c = new TrainControllerImpl();
-	private TainUser u = new TainUser();
+	private TrainUser u = new TainUser();
 	private Table<Date, reference, reference> tachografTable = ImmutableTable.<String, reference, reference> builder().put(new Date(), u.getJoystickPosition(), c.getReferenceSpeed()).build();
 	
 	//tachografTable.put(new Date(), u.getJoystickPosition(), c.getReferenceSpeed());
