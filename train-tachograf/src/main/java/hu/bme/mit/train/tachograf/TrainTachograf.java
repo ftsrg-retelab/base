95468270dae4efffbb485d6cad375c329120b837
birollaci <birollaci@gmail.com>
2022-02-27 16:19:57 +0100

    fixed a bug.

--- train-tachograf/src/main/java/hu/bme/mit/train/tachograf/TrainTachograf.java
+++ train-tachograf/src/main/java/hu/bme/mit/train/tachograf/TrainTachograf.java
@@ -1,6 +1,6 @@
 package hu.bme.mit.train.tachograf;
 
-import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainUser;
 
 import com.google.common.collect.HashBasedTable;
@@ -8,9 +8,9 @@ import com.google.common.collect.Table;
 
 public class TrainTachograf {
 
-	private TrainController c = new TrainController();
+	private TrainControllerImpl c = new TrainControllerImpl();
 	private TainUser u = new TainUser();
-	public Table<Date, reference, reference> tachografTable = ImmutableTable.<String, reference, reference> builder().put(new Date(), u.getJoystickPosition(), c.getReferenceSpeed()).build();
+	private Table<Date, reference, reference> tachografTable = ImmutableTable.<String, reference, reference> builder().put(new Date(), u.getJoystickPosition(), c.getReferenceSpeed()).build();
 	
 	//tachografTable.put(new Date(), u.getJoystickPosition(), c.getReferenceSpeed());
 	
