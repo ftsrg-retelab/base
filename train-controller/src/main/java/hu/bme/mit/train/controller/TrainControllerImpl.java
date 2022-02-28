2dad2bbb3df35e2f0c9ff44a0413a66c22cf1330
mnorbi21 <maszlagnorbert21@gmail.com>
2022-02-28 16:32:56 +0100

    intentional bug

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -36,7 +36,7 @@ public class TrainControllerImpl implements TrainController {
 	}
 
 	private void enforceSpeedLimit() {
-		if (referenceSpeed > speedLimit) {
+		if (referenceSpeed <  speedLimit) {
 			referenceSpeed = speedLimit;
 		}
 	}
