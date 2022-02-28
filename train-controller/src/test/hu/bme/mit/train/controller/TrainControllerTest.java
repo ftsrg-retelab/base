da2111962d16dfbd0638289806ad9e83c11ce44e
Coldus12 <kd123579@gmail.com>
2022-02-28 16:33:19 +0100

    oh boy

---
+++ train-controller/src/test/hu/bme/mit/train/controller/TrainControllerTest.java
@@ -0,0 +1,15 @@
+package hu.bme.mit.train.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class TrainControllerTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(1,1);
+	}
+
+}
