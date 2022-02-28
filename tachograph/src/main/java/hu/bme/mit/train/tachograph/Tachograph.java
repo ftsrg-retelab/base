8d43044d49eb701c29cc87773ebe0572ffb9bd85
Coldus12 <kd123579@gmail.com>
2022-02-28 17:05:11 +0100

    Tachograph done

--- tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
+++ tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
@@ -1,5 +1,21 @@
 package hu.bme.mit.train.tachograph;
 
+import com.google.common.collect; 
+
 public class Tachograph{
+
+    Table<Integer, Integer, Integer> tach; 
+
+    public Tachograph(){
+        tach = HashBasedTable.create(); 
+    } 
+
+    void addRecord(int t, int p, int s){
+        tach.put(t, p, s);  
+    }
     
+    int lastPos(){
+        return 2; 
+    } 
+
 } 
