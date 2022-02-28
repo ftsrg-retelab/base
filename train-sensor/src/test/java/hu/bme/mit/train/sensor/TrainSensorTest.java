3dce853b0e0e56c2c41788295589b2d92efbf2ee
mnorbi21 <maszlagnorbert21@gmail.com>
2022-02-28 16:52:41 +0100

    bux fix fix

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,5 +1,10 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.system.TrainSystem;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
