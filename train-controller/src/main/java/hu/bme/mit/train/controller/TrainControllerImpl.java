3beb1b5fdd35deaa30f4d7a4a2ec99f2ed200051
Your Name <tothbalazss1298@gmail.com>
2022-03-07 16:58:07 +0100

    newtest

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,7 +7,7 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-	private int test= 0
+	private int test= 0;
 	// some other conflict in thiw file 
 	@Override
 	public void followSpeed() {
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -16,4 +16,8 @@ public class TrainSensorTest {
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
     }
+    @Test
+    public void ThisIsAnMyTest() {
+        // TODO Delete this and add test cases based on the issues
+    }
 }
