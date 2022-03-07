37f0bb5fb5dc4d30fbebb90f79b29ce0862ea86d
Your Name <tothbalazss1298@gmail.com>
2022-03-07 16:03:08 +0100

    did some conflict in branch A

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,7 +7,7 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-
+	//random conflict
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
