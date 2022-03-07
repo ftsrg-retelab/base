fb7c71bd1b37b1d911270623fcf0f287d769c2ca
”bolingooli” <siposgergo99@gmail.com>
2022-03-07 15:50:52 +0100

    speddlimit modified

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -8,7 +8,7 @@ public class TrainSensorImpl implements TrainSensor {
 
 	private TrainController controller;
 	private TrainUser user;
-	private int speedLimit = 5;
+	private int speedLimit = 10;
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
