9c1f5c3fa29297488d2f02cc84028e9ba99189ec
remo <lacinev@gmail.com>
2022-03-07 15:46:05 +0100

    Changed Speed Limit from 0 to 1

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -6,7 +6,7 @@ public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
-	private int speedLimit = 0;
+	private int speedLimit = 1;
 
 	@Override
 	public void followSpeed() {
