600d8d2bd5c2d4a8f6080fbe7cf76f963922474a
remo <csinszka.varga@gmail.com>
2022-03-07 17:54:02 +0100

    tachograph fix

--- tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
+++ tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
@@ -4,7 +4,6 @@ import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
 import java.time.LocalDateTime;
-import hu.bme.mit.train.interfaces.Tachograph;
 
 public class Tachograph implements hu.bme.mit.train.interfaces.Tachograph {
     Table<LocalDateTime, Integer, Integer> mTable = HashBasedTable.create();
