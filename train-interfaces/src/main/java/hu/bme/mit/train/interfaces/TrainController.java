55ce560cbc052b2d733b35543edb1951405641c9
GerryCarry <39331952+GerryCarry@users.noreply.github.com>
2022-03-08 20:49:05 +0100

    Making some syntax error

--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
@@ -2,9 +2,9 @@ package hu.bme.mit.train.interfaces;
 
 public interface TrainController {
 
-	void followSpeed();
+	void followSpeed()
 
-	int getReferenceSpeed();
+	int getReferenceSpeed()
 
 	void setSpeedLimit(int speedLimit);
 
