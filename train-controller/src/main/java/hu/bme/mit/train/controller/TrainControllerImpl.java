452619854e4fe8c8b49519b3c43549c9e347ebc8
klau <b.klau4@gmail.com>
2022-05-02 15:24:38 +0200

    feature done

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,6 +7,7 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Timer timer = new Timer("Timer");
 
 	@Override
 	public void followSpeed() {
@@ -46,4 +47,28 @@ public class TrainControllerImpl implements TrainController {
 		this.step = joystickPosition;		
 	}
 
+	public void changeReferenceSpeed(){
+		TimerTask task = new TimerTask() {
+			public void run() {
+				setJoystickPosition(5);
+				referenceSpeed++;
+				while(joystickPosition > 0){
+					if(referenceSpeed< speedLimit ){
+						referenceSpeed++;
+					}
+				}
+			}
+		};
+		TimerTask task2 = new TimerTask(){
+			public void run(){
+				setJoystickPosition(-5);
+				referenceSpeed--;
+				while(joystickPosition < 0){
+					referenceSpeed++;
+				}
+			}
+		};
+		timer.schedule(task, 1000L);
+		timer.schedule(task2, 2000L);		
+	}
 }
