80fc31e6fb5275bedba4859c4fd27b735bc8f9b2
Pigeon lord <bercsenyirihi@gmail.com>
2022-05-02 16:59:11 +0200

    finished tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,47 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.user.TrainUserImpl;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
+    TrainController controller;
+    TrainSensor sensor;
+    TrainUser user;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        controller = mock(TrainController.class);
+        sensor = mock(TrainSensorImpl.class);
+        user = mock(TrainUserImpl.class);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void Test1() {
+        controller.setSpeedLimit(-1);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void Test2() {
+        controller.setSpeedLimit(1);
+        verify(user, times(0)).setAlarmState(true);
+    }
+
+    @Test
+    public void Test3() {
+        controller.setSpeedLimit(501);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void Test4() {
+        controller.setSpeedLimit(0);
+        verify(user, times(1)).setAlarmState(true);
     }
 }
