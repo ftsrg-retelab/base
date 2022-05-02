7e61aafaece4f20600c7444c952aa3da562e4a35
Pigeon lord <bercsenyirihi@gmail.com>
2022-05-02 16:39:41 +0200

    maybe?

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -22,8 +22,11 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+		if (speedLimit < 0 || speedLimit > 500)
+			user.setAlarmState(true);
+		if (controller.getReferenceSpeed() > speedLimit * 1.5)
+			user.setAlarmState(true);
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
 	}
-
 }
--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -7,6 +7,7 @@ public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private boolean alarmState;
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
@@ -28,4 +29,13 @@ public class TrainUserImpl implements TrainUser {
 		controller.setJoystickPosition(joystickPosition);
 	}
 
+	@Override
+	public boolean getAlarmState(){
+		return alarmState;
+	}
+
+	@Override
+	public void setAlarmState(boolean alarmState){
+		this.alarmState = alarmState;
+	}
 }
