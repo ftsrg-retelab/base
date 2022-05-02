347a481b3038395e02dab0dc12ff389a96f54570
held-n <held.noemi@gmail.com>
2022-05-02 16:38:22 +0200

    message2

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,7 +1,6 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
-//import hu.bme.mit.train.tachograph.*;
 
 public class TrainControllerImpl implements TrainController {
 
