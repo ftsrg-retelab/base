026a1d4d34d9fddfc4308be629a73ab3d5053dd8
damenija7 <damenija7@gmail.com>
2022-05-02 14:50:35 +0200

    TrainControllerImpl now changes reference speed periodically

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,31 +1,53 @@
 package hu.bme.mit.train.controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
+	private Object referenceSpeedSyncObj = new Object();
 	private int speedLimit = 0;
+	
+	private TimerTask followSpeedTask = new TimerTask() {
+		@Override
+		public void run() {
+			followSpeed();
+		}
+	};
+	private Timer timer = new Timer();
+	
+	
+	public TrainControllerImpl() {
+		// 1 mp mulva folyamatosan hivogasd 1 mp-kent
+		timer.schedule(followSpeedTask, 1000, 1000);
+	}
 
 	@Override
 	public void followSpeed() {
-		if (referenceSpeed < 0) {
-			referenceSpeed = 0;
-		} else {
-		    if(referenceSpeed+step > 0) {
-                referenceSpeed += step;
-            } else {
-		        referenceSpeed = 0;
-            }
+		synchronized(referenceSpeedSyncObj) {
+			if (referenceSpeed < 0) {
+				referenceSpeed = 0;
+			} else {
+			    if(referenceSpeed+step > 0) {
+	                referenceSpeed += step;
+	            } else {
+			        referenceSpeed = 0;
+	            }
+			}
+	
+			enforceSpeedLimit();
 		}
-
-		enforceSpeedLimit();
 	}
 
 	@Override
 	public int getReferenceSpeed() {
-		return referenceSpeed;
+		synchronized(referenceSpeedSyncObj) {
+			return referenceSpeed;
+		}
 	}
 
 	@Override
@@ -36,14 +58,18 @@ public class TrainControllerImpl implements TrainController {
 	}
 
 	private void enforceSpeedLimit() {
-		if (referenceSpeed > speedLimit) {
-			referenceSpeed = speedLimit;
+		synchronized(referenceSpeedSyncObj) {
+			if (referenceSpeed > speedLimit) {
+				referenceSpeed = speedLimit;
+			}
 		}
 	}
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		synchronized(referenceSpeedSyncObj) {
+			this.step = joystickPosition;		
+		}
 	}
 
 }
