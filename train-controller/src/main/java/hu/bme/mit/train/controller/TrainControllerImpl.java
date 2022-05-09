a1cb6eb92523c360400a4a79d4b0b587852cb4a4
Nagy Marcell <nmarcell05@gmail.com>
2022-05-09 15:04:21 +0200

    playerA impl

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -8,6 +8,20 @@ public class TrainControllerImpl implements TrainController {
 	private int referenceSpeed = 0;
 	private int speedLimit = 60;
 
+	public TrainControllerImpl() {
+		thread = new Thread() {  
+			public void run() {
+				thread.run();
+				try {
+					followSpeed();
+					thread.sleep(2000); 
+				} catch(InterruptedException e) {
+					e.printStackTrace(); 
+				}  
+			} 
+		}	
+	} 
+
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
