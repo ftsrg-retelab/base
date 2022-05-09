07f22c62baef6447526924c2d4e653f787840188
onandor <olahnandi00@gmail.com>
2022-05-09 13:51:47 +0200

    Added reference speed increment timer

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,12 +1,21 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
+import java.util.TimerTask;
+import java.util.Timer;  
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	boolean timerStarted = false; 
+	Timer timer = new Timer();
+	TimerTask task = new TimerTask() {
+		public void run() {
+			followSpeed(); 
+		} 
+	};
 
 	@Override
 	public void followSpeed() {
@@ -49,7 +58,11 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
+		if (!timerStarted) {
+			timer.scheduleAtFixedRate(task, 0, 1000);
+			timerStarted = true; 
+		} 
 	}
 
 }
