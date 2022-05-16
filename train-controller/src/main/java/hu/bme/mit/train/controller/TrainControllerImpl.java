77c9e8d053362bd1fb36b59f261590b62becc9b5
Apollo12345678 <weiszapollo@gmail.com>
2022-05-16 14:56:05 +0200

    test commit

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,12 +1,28 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Timer time;r
+
+	public TrainControllerImpl(){
+       Timer timer = new Timer();
+       timer.schedule(new TimerTask(){
+           @Override
+           public void run(){
+               followSpeed();
+           }
+       }, 0, 1000);
+   }
+
+
 
 	@Override
 	public void followSpeed() {
