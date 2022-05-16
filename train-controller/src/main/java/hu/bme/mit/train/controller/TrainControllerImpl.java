76faefdd1e43926a9a4137f4a02b388989907362
Tarany Peter <kijevigombooc@gmail.com>
2022-05-16 15:32:57 +0200

    Changed time

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -15,7 +15,7 @@ public class TrainControllerImpl implements TrainController {
 				while (true) {
 					followSpeed();
 					try {
-						Thread.sleep(100);
+						Thread.sleep(1000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
