74cb890c58c17ea28778321b2ec3e5b633845868
asztrikx <asztrik35@gmail.com>
2023-02-12 11:42:08 +0100

    fix using var

--- train-tachograph/src/test/java/hu/bme/mit/train/tachograph/TachographTest.java
+++ train-tachograph/src/test/java/hu/bme/mit/train/tachograph/TachographTest.java
@@ -9,7 +9,7 @@ public class TachographTest {
 	@Test
 	public void OverridingJoystickPosition_IncreasesReferenceSpeed() {
 		Tachograph x = new Tachograph();
-		var time = LocalDateTime.now();
+		LocalDateTime time = LocalDateTime.now();
 		x.add(time, 1.0, 2.0, 3.0);
 		Double[] c = x.get(time, 3.0);
 		Assert.assertEquals(1.0, (double)c[0], 0.00);
