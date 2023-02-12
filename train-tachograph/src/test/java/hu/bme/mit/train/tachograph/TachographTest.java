d0204e99dbc5ee672feb018bed57f28761f02824
asztrikx <asztrik35@gmail.com>
2023-02-12 11:41:04 +0100

    add test

---
+++ train-tachograph/src/test/java/hu/bme/mit/train/tachograph/TachographTest.java
@@ -0,0 +1,18 @@
+package hu.bme.mit.train.tachograph;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+public class TachographTest {
+	@Test
+	public void OverridingJoystickPosition_IncreasesReferenceSpeed() {
+		Tachograph x = new Tachograph();
+		var time = LocalDateTime.now();
+		x.add(time, 1.0, 2.0, 3.0);
+		Double[] c = x.get(time, 3.0);
+		Assert.assertEquals(1.0, (double)c[0], 0.00);
+		Assert.assertEquals(2.0, (double)c[1], 0.00);
+	}
+}
