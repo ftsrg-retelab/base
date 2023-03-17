5d9cd4d34062b224015bddb1a262b34c27b6a66e
Jenga05 <80204216+Jenga05@users.noreply.github.com>
2023-03-18 00:09:32 +0100

    add new environmental sensor

---
+++ train-environment-sensor/src/main/java/hu/bme/mit/train/sensor/TrainEnvironmentSensorImpl.java
@@ -0,0 +1,29 @@
+package hu.bme.mit.train.sensor;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+
+public class TrainSensorImpl implements TrainSensor {
+
+	private TrainController controller;
+	private TrainUser user;
+	private int speedLimit = 5;
+
+	public TrainSensorImpl(TrainController controller, TrainUser user) {
+		this.controller = controller;
+		this.user = user;
+	}
+
+	@Override
+	public int getSpeedLimit() {
+		return speedLimit;
+	}
+
+	@Override
+	public void overrideSpeedLimit(int speedLimit) {
+		this.speedLimit = speedLimit;
+		controller.setSpeedLimit(speedLimit);
+	}
+
+}
---
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainEnvironmentSensor.java
@@ -0,0 +1,9 @@
+package hu.bme.mit.train.interfaces;
+
+public interface TrainSensor {
+
+	int getSpeedLimit();
+
+	void overrideSpeedLimit(int speedLimit);
+
+}
