046d454143e3083de5c67075f98f9c647ea4d402
boa <boa@protonmail.com>
2023-03-20 13:24:45 +0100

    Fix indent

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -13,11 +13,11 @@ public class TrainControllerImpl implements TrainController {
 		if (referenceSpeed < 0) {
 			referenceSpeed = 0;
 		} else {
-		    if(referenceSpeed+step > 0) {
-                referenceSpeed += step;
-            } else {
-		        referenceSpeed = 0;
-            }
+			if(referenceSpeed+step > 0) {
+				referenceSpeed += step;
+			} else {
+				referenceSpeed = 0;
+			}
 		}
 
 		enforceSpeedLimit();
