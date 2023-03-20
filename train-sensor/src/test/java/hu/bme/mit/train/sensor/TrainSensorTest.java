e602f25fb3b345b9017dac4829e4792c8fee7bec
Breinich <bajnokvencel@edu.bme.hu>
2023-03-20 16:14:54 +0100

    bug fix + uj teszt

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -16,4 +16,9 @@ public class TrainSensorTest {
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
     }
+
+    @Test
+    public void ThisIsAnExampleTestStub2() {
+        // TODO Delete this and add test cases based on the issues
+    }
 }
