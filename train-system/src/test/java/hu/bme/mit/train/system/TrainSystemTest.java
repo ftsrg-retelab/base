496884e5a7570dbc877e2af47e0fa98ba3ed9b42
Egi2018 <kovacsegon806gmail.com>
2023-03-20 17:40:00 +0100

    Teszt kész

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,11 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+	public void overrideSpeedLimit() {
+		controller.emergencyBreaking();
+		Assert.assertEquals(0, controller.getReferenceSpeed());
+	}
+
 	
 }
