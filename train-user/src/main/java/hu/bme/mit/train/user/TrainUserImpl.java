4248b9719336f88e774e76b4aef755a56e543cfc
Breinich <bajnokvencel@edu.bme.hu>
2023-03-20 17:50:54 +0100

    user import fix

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -2,7 +2,6 @@ package hu.bme.mit.train.user;
 
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.tachograf.Tacho;
 
 public class TrainUserImpl implements TrainUser {
 
