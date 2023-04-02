ab25b8528e9fc08b7e7c8014b02fbfa66c1704c8
Fcccdx <nemeth.a.fabian@gmail.com>
2023-04-02 19:26:40 +0200

    Emergency brake implemented

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,9 +7,25 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private boolean emergencybrake = false;
 
 	@Override
 	public void followSpeed() {
+		if(this.emergencybrake == true)
+		{
+			while(referenceSpeed != 0)
+			{
+				if(referenceSpeed > 0)
+					referenceSpeed--;
+				if(referenceSpeed < 0)
+					referenceSpeed++;
+				else
+				{
+					setJoystickPosition(0);
+					break;
+				}
+			}
+		}
 		if (referenceSpeed < 0) {
 			referenceSpeed = 0;
 		} else {
