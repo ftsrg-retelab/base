b96497fe368289ec9d6a6897022117182e367040
dh-anna <dh01anna@gmail.com>
2023-04-17 17:00:22 +0200

    feat: timer

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -6,15 +6,18 @@ import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
+import java.util.Timer;
 
 public class TrainSystem {
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+	private Timer = new Timer();
 
 	public TrainController getController() {
 		return controller;
+		timer.scheduleAtFixedRate(() -> followSpeed(), 2*60*1000, 2*60*1000);
 	}
 
 	public TrainSensor getSensor() {
