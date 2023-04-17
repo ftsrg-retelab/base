736d95baef39767b1dd157cca7f8b1d8bd847fbb
LaszloAbrok <laszlopatrikabrok@gmail.com>
2023-04-17 16:50:03 +0200

    ref speed mod

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,7 +2,7 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
-public class TrainControllerImpl implements TrainController {
+public class TrainControllerImpl implements TrainController, Runnable {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
@@ -45,11 +45,24 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
+		try {
+			Thread.sleep(100);	
+		} catch (Exception e) {
+			System.out.println("thread exception");
+		}
+			
 	}
 
 	@Override
 	public void emergencyBrake(){
 		this.referenceSpeed=0;
+	}
+
+
+	public void run(){
+		while(true){
+			followSpeed();
+		}
 	}
 }
