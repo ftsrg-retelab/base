e209b510d4c61b5b38aa67579f42ecd6242611b0
Phextod <illes980422@gmail.com>
2023-04-18 11:46:46 +0200

    Reference speed updating implemented

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,6 +2,8 @@ package hu.bme.mit.train.controller;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
@@ -13,6 +15,17 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private final Timer refSpeedUpdateTimer;
+
+	public TrainControllerImpl(){
+		refSpeedUpdateTimer = new Timer();
+		refSpeedUpdateTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				followSpeed();
+			}
+		} , 0, 1000);
+	}
 
 	@Override
 	public void followSpeed() {
