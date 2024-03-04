4221c516b7a01e1fb2f354c920f778eb068d1c8c
zolcsakgergo <zolcsakgergo2001@gmail.com>
2024-03-04 18:13:30 +0100

    fixes

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,31 +2,14 @@ package hu.bme.mit.train.controller;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
-
 import hu.bme.mit.train.interfaces.TrainController;
-
 public class TrainControllerImpl implements TrainController {
-
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-	private final Timer refSpeedUpdateTimer_v1;
-
-	public TrainControllerImpl(){
-		refSpeedUpdateTimer_v1 = new Timer();
-		refSpeedUpdateTimer_v1.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				followSpeed();
-			}
-		} , 0, 1000);
-	}
-
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
@@ -38,42 +21,35 @@ public class TrainControllerImpl implements TrainController {
 		        referenceSpeed = 0;
             }
 		}
-
 		enforceSpeedLimit();
 	}
-
 	@Override
 	public int getReferenceSpeed() {
 		return referenceSpeed;
 	}
-
 	@Override
 	public void setSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
 		enforceSpeedLimit();
 		
 	}
-
 	private void enforceSpeedLimit() {
 		if (referenceSpeed > speedLimit) {
 			referenceSpeed = speedLimit;
 		}
 	}
-
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
 		this.step = joystickPosition;		
 	}
-
 	@Override
 	public void setSpeedToNull(){
 		setSpeedLimit(0);
 		enforceSpeedLimit();
 	}
-
 	@Override
 	public void emergencyBreaking() {
 		setSpeedToNull();
 	}
-	
+
 }
