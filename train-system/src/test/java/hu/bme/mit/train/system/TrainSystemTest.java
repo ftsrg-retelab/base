bcc51b2c77413fcff1b5a252d66d1610e815a027
bertokd <bertokd57@gmail.com>
2024-03-04 17:28:40 +0100

    uj teszt

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,10 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+    public void speedLimitTest(){
+        assertEquals(1, 1);
+    }
+
 	
 }
