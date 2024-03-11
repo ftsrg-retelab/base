177d67c10101bf1aa95efa471ce2b4b8c8389200
csongorczibulya33 <csongorczibulya33@gmail.com>
2024-03-11 17:01:22 +0100

    vészfék implementálása javitás3

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -26,9 +26,7 @@ public class TrainUserImpl implements TrainUser {
 	public void overrideJoystickPosition(int joystickPosition) {
 		this.joystickPosition = joystickPosition;
 		controller.setJoystickPosition(joystickPosition);
-		/*if (joystickPosition == 0){
-			emergencyBrake();
-		}*/
+		
 	}
 
 
