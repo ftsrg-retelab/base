ee07e1cbeb4b1cae2dc2fbc55b7b82c78e17e31f
Vörös Gergely <gergo009@gmail.com>
2024-03-11 11:20:41 +0100

    Feature implementation and mvn test

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -4,46 +4,67 @@ import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
 
-	private int step = 0;
-	private int referenceSpeed = 0;
-	private int speedLimit = 0;
+    private int step = 0;
+    private int referenceSpeed = 0;
+    private int speedLimit = 0;
 
-	@Override
-	public void followSpeed() {
-		if (referenceSpeed < 0) {
-			referenceSpeed = 0;
-		} else {
-		    if(referenceSpeed+step > 0) {
+    // Flag to indicate whether speed limit has been reached
+    private boolean speedLimitReached = false;
+
+    @Override
+    public void followSpeed() {
+        if (referenceSpeed < 0) {
+            referenceSpeed = 0;
+        } else {
+            if (referenceSpeed + step > 0) {
                 referenceSpeed += step;
             } else {
-		        referenceSpeed = 0;
+                referenceSpeed = 0;
             }
-		}
+        }
 
-		enforceSpeedLimit();
-	}
+        enforceSpeedLimit();
 
-	@Override
-	public int getReferenceSpeed() {
-		return referenceSpeed;
-	}
+        // Check if speed limit has been reached
+        if (referenceSpeed == speedLimit) {
+            speedLimitReached = true;
+        } else {
+            speedLimitReached = false;
+        }
+    }
 
-	@Override
-	public void setSpeedLimit(int speedLimit) {
-		this.speedLimit = speedLimit;
-		enforceSpeedLimit();
-		
-	}
+    @Override
+    public int getReferenceSpeed() {
+        return referenceSpeed;
+    }
 
-	private void enforceSpeedLimit() {
-		if (referenceSpeed > speedLimit) {
-			referenceSpeed = speedLimit;
-		}
-	}
+    @Override
+    public void setSpeedLimit(int speedLimit) {
+        this.speedLimit = speedLimit;
+        enforceSpeedLimit();
 
-	@Override
-	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
-	}
+        // Check if speed limit has been reached after updating speed limit
+        if (referenceSpeed == speedLimit) {
+            speedLimitReached = true;
+        } else {
+            speedLimitReached = false;
+        }
+    }
 
+    private void enforceSpeedLimit() {
+        if (referenceSpeed > speedLimit) {
+            referenceSpeed = speedLimit;
+        }
+    }
+
+    @Override
+    public void setJoystickPosition(int joystickPosition) {
+        this.step = joystickPosition;
+    }
+
+    // Method to check if speed limit has been reached
+    @Override
+    public boolean isSpeedLimitReached() {
+        return speedLimitReached;
+    }
 }
