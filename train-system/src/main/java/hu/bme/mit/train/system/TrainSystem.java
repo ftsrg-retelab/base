1aa1b5a9c66cf129433b6494950fc72161c90a76
atiabri <mataabri@gmail.com>
2024-03-15 17:29:56 +0100

    lab3 first

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -6,6 +6,9 @@ import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class TrainSystem {
 
@@ -25,4 +28,15 @@ public class TrainSystem {
 		return user;
 	}
 
+	public void startSpeedUpdateTimer() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                // Get joystick position and update reference speed
+                int joystickPosition = user.getJoystickPosition();
+                controller.setJoystickPosition(joystickPosition);
+            }
+        }, 0, 1000); // Update every second
+    }
 }
