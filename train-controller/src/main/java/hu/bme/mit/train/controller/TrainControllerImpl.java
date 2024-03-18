8b11eefe2d9d2e929817ce94c626c1b7b029b243
Marton Csutora <marton@csutora.com>
2024-03-18 16:20:49 +0100

    implemented feature

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,29 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer; 
+import java.util.TimerTask; 
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	class FunctionTimer extends TimerTask 
+	{ 
+		public void run() 
+		{ 
+			followSpeed();
+		}
+	}
+
+	public TrainControllerImpl() {
+		Timer timer = new Timer();
+		TimerTask task = new FunctionTimer();
+
+		timer.schedule(task, 0, 1000);
+	}
 
 	@Override
 	public void followSpeed() {
