293bf0288dadb2a6d074b3f17a762fe008e7fe12
zolcsakgergo <zolcsakgergo2001@gmail.com>
2024-03-18 16:33:34 +0100

    trainController implemented using Thread

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -10,6 +10,25 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Thread t;
+
+	public TrainControllerImpl(){
+		t = new Thread() {
+			public void run(){
+				t.run();
+				try{
+					followSpeed();
+					thread.sleep(2000);
+				}catch (InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+		};
+	}
+
+
+
+
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
